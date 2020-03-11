58b11d4fb266a530ae62bc9d8852576d7147f5c2
Brando <brando.perez.pacheco@gmail.com>
2020-03-10 19:08:50 -0500

    added prefix patsh

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: `/springdeal`,
   siteMetadata: {
     title: `HipHotel Tulum`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
